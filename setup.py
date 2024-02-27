65b369184e1b367d9e425e7a85f82bef045944b7
Markus Schulze <49436733+fxhuhn@users.noreply.github.com>
2024-02-27 16:09:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tiny_ta',
-    version='0.0.2',
+    version='0.0.3',
     description='some methods for technical analysis!',
 
     url='https://github.com/fxhuhn/tiny_ta',
